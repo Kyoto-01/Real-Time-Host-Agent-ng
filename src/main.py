65aa5ce9d8131fce27df1c0195b8c69e0f069088
Kyoto-01 <cameraman269@gmail.com>
2022-10-21 22:43:12 -0300

    feat: mechanism to specify informations that you want to get

--- src/main.py
+++ src/main.py
@@ -1,5 +1,41 @@
-from collector.resources import *
-from pprint import pprint
+from flask import Flask, request
+from collector.resources import ResMachine
 
-local = ResMachine()
-pprint(local.show(), sort_dicts=False)
+app = Flask(__name__)
+host = '0.0.0.0'
+port = 5000
+
+machine = ResMachine()
+
+@app.route('/join')
+def join_manager():
+    pass
+
+@app.post('/get')
+def get_state():
+    # List of requested machine informations
+    paths = request.json['get']
+
+    # Current machine state
+    state = machine.collect()
+
+    # Requested machine informations
+    res = {}
+
+    for path in paths:
+        data = state
+
+        for key in path.split('.'):
+            if key in data:
+                data = data[key]
+                
+        res[path] = data
+
+    return res
+
+
+@app.post('/set')
+def set_state():
+    pass
+
+app.run(host=host, port=port)
